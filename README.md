8a5387163b83fc64dc7601390cd35c0c9c7111cc
wesker <uerax@live.com>
2023-12-06 10:05:20 +0800

    feat: add ssh.sh

--- README.md
+++ README.md
@@ -12,6 +12,12 @@ __挖矿脚本__
 bash -c "$(curl -L https://cdn.jsdelivr.net/gh/uerax/script@master/mining.sh)" @
 ```
 
+__一键修改root密码和端口__
+
+```
+bash -c "$(curl -L https://cdn.jsdelivr.net/gh/uerax/script@master/ssh.sh)" @
+```
+
 __mac工具备份脚本__
 
 ```
---
+++ ssh.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ $(id -u) == 0 ]; then
+    echo -e "进入安装流程"
+    sleep 3
+else
+    echo -e  "请切使用root用户执行脚本"
+    echo -e  "切换root用户命令: sudo su"
+    exit 1
+fi
+
+apt autoremove -y --purge needrestart
+apt update && apt install -y vim
+
+echo -e "========================================"
+echo -e "输入你的密码: "
+passwd root
+
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+
+port=2222
+echo -e "输入SSH要设置的端口(默认2222)"
+read -rp "请输入: " input
+if [[ $input =~ ^[0-9]+$ ]] && (($input >= 1 && $input <= 65535)); then
+    port=${input}
+fi
+
+cat >> /etc/ssh/sshd_config <<EOF
+Port ${port}
+EOF
+
+service sshd restart
