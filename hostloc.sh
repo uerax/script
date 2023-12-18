34335413e8e4801869c31944a466d7a93a458c13
wesker <uerax@live.com>
2023-12-19 02:29:37 +0800

    feat: remove nodeseek

--- hostloc.sh
+++ hostloc.sh
@@ -25,25 +25,3 @@ curl "https://hostloc.com/space-uid-$id.html" \
 
   sleep 0.5
 done
-
-
-# nodeseek
-curl 'https://www.nodeseek.com/api/attendance?random=true' \
-  -X 'POST' \
-  -H 'cookie: ' \
-  -H 'authority: www.nodeseek.com' \
-  -H 'accept: */*' \
-  -H 'accept-language: zh-CN,zh;q=0.9,en;q=0.8' \
-  -H 'cache-control: no-cache' \
-  -H 'content-length: 0' \
-  -H 'origin: https://www.nodeseek.com' \
-  -H 'pragma: no-cache' \
-  -H 'referer: https://www.nodeseek.com/board' \
-  -H 'sec-ch-ua: "Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"' \
-  -H 'sec-ch-ua-mobile: ?0' \
-  -H 'sec-ch-ua-platform: "Windows"' \
-  -H 'sec-fetch-dest: empty' \
-  -H 'sec-fetch-mode: cors' \
-  -H 'sec-fetch-site: same-origin' \
-  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' \
-  --compressed
