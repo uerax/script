66b59485676a7b938e218a833549ff11876184d4
rotlands <uerax@live.com>
2024-08-25 19:25:13 +0800

    feat: vim

--- oracle.sh
+++ oracle.sh
@@ -3,4 +3,9 @@ cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys
 apt purge needrestart -y
 apt install vim -y
 
+cat >> ~/.vimrc <<EOF
+:set mouse-=a
+syntax on
+EOF
+
 bash -c "$(curl -L https://cdn.jsdelivr.net/gh/uerax/script@master/bashrc.sh)" @
