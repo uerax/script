df2fa639493a5a27d783762ac747aee1407b47bd
wesker <uerax@live.com>
2024-08-31 21:54:07 +0800

    feat: add ipv4 first

--- oracle.sh
+++ oracle.sh
@@ -8,4 +8,6 @@ cat >> ~/.vimrc <<EOF
 syntax on
 EOF
 
+sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/gai.conf
+
 bash -c "$(curl -L https://cdn.jsdelivr.net/gh/uerax/script@master/bashrc.sh)" @
