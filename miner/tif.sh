484af4487f332f5be263b2652ffc7418c3f33141
wesker <uerax@live.com>
2024-10-11 08:17:46 +0800

    feat: add upgrade

--- miner/tif.sh
+++ miner/tif.sh
@@ -5,87 +5,88 @@ server_name="ueraxext"
 nproc=$(nproc)
 workers=$nproc
 
-if ! command -v figlet &> /dev/null; then
-    echo "figlet is not installed. Installing..."
-    sudo apt-get install figlet -y
-fi
-
-echo "Server name accepted: $server_name"
-echo "Make sure this name is unique to avoid conflicts with other benchmarkers."
-
-concat="${address}_${server_name}"
-echo "The concatenation of the address and name is: $concat"
-
-if [ -d "/root/tif-miningpool" ]; then
-    echo "The tif-miningpool directory already exists. Deleting it..."
-    rm -rf /root/tif-miningpool
-fi
-
-echo "Creating the tif-miningpool directory..."
-mkdir -p /root/tif-miningpool
-cd /root/tif-miningpool || exit
-
-echo "Generating the information file..."
-
-echo "address = $address" > address_name
-echo "name = $server_name" >> address_name
-
-echo "File address_name created in /root/tif-miningpool"
-
-echo "Cloning the tig-monorepo repository..."
-git clone https://github.com/tig-foundation/tig-monorepo.git
-
-echo "Updating packages..."
-apt-get update -y
-
-install_python() {
-    version=$1
-    echo "Attempting to install Python $version..."
-    if apt-get install -y python$version python$version-venv; then
-        echo "Python $version installed successfully."
-        return 0
-    else
-        echo "Failed to install Python $version."
-        return 1
+install_benchmarker() {
+    if ! command -v figlet &> /dev/null; then
+        echo "figlet is not installed. Installing..."
+        sudo apt-get install figlet -y
     fi
-}
 
-python_versions=("3.12" "3.11" "3.10" "3.9" "3.8")
-for version in "${python_versions[@]}"; do
-    if install_python $version; then
-        python_version=$version
-        break
+    echo "Server name accepted: $server_name"
+    echo "Make sure this name is unique to avoid conflicts with other benchmarkers."
+
+    concat="${address}_${server_name}"
+    echo "The concatenation of the address and name is: $concat"
+
+    if [ -d "/root/tif-miningpool" ]; then
+        echo "The tif-miningpool directory already exists. Deleting it..."
+        rm -rf /root/tif-miningpool
     fi
-done
 
-if [ -z "$python_version" ]; then
-    echo "Unable to install a compatible Python version. Stopping the script."
-    exit 1
-fi
+    echo "Creating the tif-miningpool directory..."
+    mkdir -p /root/tif-miningpool
+    cd /root/tif-miningpool || exit
 
-echo "Creating and activating the virtual environment with Python $python_version..."
-python$python_version -m venv /root/tif-miningpool/myenv
-source /root/tif-miningpool/myenv/bin/activate
+    echo "Generating the information file..."
 
-echo "Installing Python dependencies..."
-python$python_version -m pip install -r ./tig-monorepo/tig-benchmarker/requirements.txt
-python$python_version -m pip install requests
+    echo "address = $address" > address_name
+    echo "name = $server_name" >> address_name
 
-echo "Installing Cargo and rustup..."
-apt-get install -y cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
+    echo "File address_name created in /root/tif-miningpool"
 
-echo "Compiling tig-worker..."
-cd tig-monorepo || exit
-cargo build -p tig-worker --release
+    echo "Cloning the tig-monorepo repository..."
+    git clone https://github.com/tig-foundation/tig-monorepo.git
 
-echo "chmod on tig-worker files..."
-chmod +x ./target/release/tig-worker
+    echo "Updating packages..."
+    apt-get update -y
 
-echo "Creating systemd service for TIG Worker..."
+    install_python() {
+        version=$1
+        echo "Attempting to install Python $version..."
+        if apt-get install -y python$version python$version-venv; then
+            echo "Python $version installed successfully."
+            return 0
+        else
+            echo "Failed to install Python $version."
+            return 1
+        fi
+    }
 
-cat << EOF > /etc/systemd/system/tif-miningpool.service
+    python_versions=("3.12" "3.11" "3.10" "3.9" "3.8")
+    for version in "${python_versions[@]}"; do
+        if install_python $version; then
+            python_version=$version
+            break
+        fi
+    done
+
+    if [ -z "$python_version" ]; then
+        echo "Unable to install a compatible Python version. Stopping the script."
+        exit 1
+    fi
+
+    echo "Creating and activating the virtual environment with Python $python_version..."
+    python$python_version -m venv /root/tif-miningpool/myenv
+    source /root/tif-miningpool/myenv/bin/activate
+
+    echo "Installing Python dependencies..."
+    python$python_version -m pip install -r ./tig-monorepo/tig-benchmarker/requirements.txt
+    python$python_version -m pip install requests
+
+    echo "Installing Cargo and rustup..."
+    apt-get install -y cargo
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source "$HOME/.cargo/env"
+
+    echo "Compiling tig-worker..."
+    cd tig-monorepo || exit
+    cargo build -p tig-worker --release
+
+    echo "chmod on tig-worker files..."
+    chmod +x ./target/release/tig-worker
+
+    echo "Creating systemd service for TIG Worker..."
+
+    cat << EOF > /etc/systemd/system/tif-miningpool.service
 [Unit]
 Description=Tif-Benchmarking
 After=network.target
@@ -101,4 +102,34 @@ Restart=always
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload
+    systemctl daemon-reload
+}
+
+update_benchmarker() {
+    if [ -d "/root/tif-miningpool" ]; then
+        echo "Stopping the current benchmarker service..."
+        systemctl stop tif-miningpool.service
+
+        cd /root/tif-miningpool/tig-monorepo/tig-benchmarker || {
+            echo "Unable to find the benchmarker directory. The Innovation Forge might not be installed correctly."
+            return
+        }
+        echo "Updating the benchmarker..."
+        git fetch --all
+        git reset --hard origin/main
+
+        echo "Restarting the benchmarker service..."
+        systemctl start tif-miningpool.service
+    else
+        echo "The Innovation Forge is not installed."
+    fi
+}
+
+case $1 in
+    update)
+        update_benchmarker
+        ;;
+    *)
+        install_benchmarker
+        ;;
+esac
