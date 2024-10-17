8965c2431d1a43c685954995b0736a254bdde922
wesker <uerax@live.com>
2024-10-18 04:02:27 +0800

    fix: grpc

--- miner/quilibrium.sh
+++ miner/quilibrium.sh
@@ -61,6 +61,20 @@ WantedBy=multi-user.target
 EOF
     systemctl daemon-reload
     systemctl start quili
+    systemctl stop quili
+
+    listenGrpcMultiaddr="/ip4/127.0.0.1/tcp/8337"
+    listenRESTMultiaddr="/ip4/127.0.0.1/tcp/8338"
+    statsMultiaddr="/dns/stats.quilibrium.com/tcp/443"
+
+    sleep 1
+
+    sed -i "s~^\(\s*\)listenGrpcMultiaddr:.*~\1listenGrpcMultiaddr: \"${listenGrpcMultiaddr}\"~" ~/ceremonyclient/node/.config/config.yml
+    sed -i "s~^\(\s*\)listenRESTMultiaddr:.*~\1listenRESTMultiaddr: \"${listenRESTMultiaddr}\"~" ~/ceremonyclient/node/.config/config.yml
+    sed -i "s~^\(\s*\)statsMultiaddr:.*~\1statsMultiaddr: \"${statsMultiaddr}\"~" ~/ceremonyclient/node/.config/config.yml
+
+    systemctl restart quili
+    
 }
 
 run() {
