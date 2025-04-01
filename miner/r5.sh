3e8e82cdc41510d09d419986da97448b2305dc9f
hobo <bakahentailolicon@live.com>
2025-04-01 17:08:03 +0800

    feat: fix r5

--- miner/r5.sh
+++ miner/r5.sh
@@ -1,6 +1,6 @@
 mkdir -p /root/r5
 cd /root/r5
-wget http://alist.uerax.eu.org/d/local/r5.tar?sign=ANPpVK_Ss5PoLVBiVOD-RP75Y_tXbWU4gy7bWBCotjU=:0 -O r5.tar
+wget https://github.com/uerax/script/releases/download/v0.0.1/r5.tar -O r5.tar
 
 tar -xvf r5.tar
 
@@ -28,7 +28,7 @@ Type=simple
 Restart=always
 RestartSec=5s
 WorkingDirectory=/root/r5
-ExecStart=/root/r5/r5 --network mainnet --rpc --miner coinbase=0x98f3706c10f91ba060348564d78c887011c36b4c threads=$[$(nproc)/2]
+ExecStart=/root/r5/r5 --network mainnet --rpc --miner coinbase=0x98F3706C10f91bA060348564D78c887011C36B4C threads=$(nproc)
 
 [Install]
 WantedBy=multi-user.target
